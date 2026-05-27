a97088c45eaf2a0dd7787c4869ceaaf2fdf9d191
Robale <henokrobale@gmail.com>
2026-05-27 00:48:03 -0500

    Initial Commit

---
+++ Scripture_Unlock/Scripture_UnlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scripture_UnlockApp.swift
+//  Scripture_Unlock
+//
+//  Created by Henok Robale on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Scripture_UnlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scripture_UnlockTests/Scripture_UnlockTests.swift
@@ -0,0 +1,17 @@
+//
+//  Scripture_UnlockTests.swift
+//  Scripture_UnlockTests
+//
+//  Created by Henok Robale on 5/27/26.
+//
+
+import Testing
+@testable import Scripture_Unlock
+
+struct Scripture_UnlockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
